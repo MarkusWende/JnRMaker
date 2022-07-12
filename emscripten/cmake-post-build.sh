f78fc065699053de443a93e99ee7d8812ee94a05
Markus Wende <markuswende@hotmail.com>
2022-07-12 12:07:00 +0200

    Removed old commands from build script

--- emscripten/cmake-post-build.sh
+++ emscripten/cmake-post-build.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Copy our 'index.html' and 'favicon.ico' into the output folder.
-cp templates/index.html build/index.html
-cp templates/favicon.ico build/favicon.ico
-cp templates/server.py build/server.py
-cp templates/key.pem build/key.pem
-cp templates/cert.pem build/cert.pem
-
-# Navigate into the output folder then start a simple server and open it.
-cd build
-/usr/bin/python3 server.py
-#python3 -m http.server
-
-#!/bin/bash
-
 # Copy to server
 rm -rf ../../JnRMakerServer/build/www/*
 cp build/jnrmaker* ../../JnRMakerServer/build/www/
