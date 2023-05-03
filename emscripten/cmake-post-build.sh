b301706738d62864d61f171173e6dbfc13976eb6
Markus Wende <markuswende@hotmail.com>
2023-05-03 20:03:23 +0200

    Fixing building errors

--- emscripten/cmake-post-build.sh
+++ emscripten/cmake-post-build.sh
@@ -2,7 +2,7 @@
 
 # Copy to server
 rm -rf ../../JnRMakerServer/build/www/*
-cp build/jnrmaker* ../../JnRMakerServer/build/www/
+cp build/out/jnrmaker* ../../JnRMakerServer/build/www/
 cp ../resources/www/index.html ../../JnRMakerServer/build/www/
 cp ../resources/www/favicon.ico ../../JnRMakerServer/build/www/
 
