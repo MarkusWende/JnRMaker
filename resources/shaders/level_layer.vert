6555ef6cc60638fe269f645bdb5982a7c8ddf02c
Markus Wende <markuswende@hotmail.com>
2023-05-31 12:11:52 +0200

    changed gitignore

---
+++ resources/shaders/level_layer.vert
@@ -0,0 +1,25 @@
+#version 330 core
+
+layout (location = 0) in vec2 aPos;
+layout (location = 1) in vec2 aTexCoords;
+layout (location = 2) in vec2 aTileTrans;
+layout (location = 3) in float aTileID;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+out vec2 TexCoords;
+out vec3 FragPos;
+out vec3 Normal;
+flat out int instanceID;
+out float tileID;
+
+void main()
+{
+    vec2 offset = vec2(aTileTrans.x, aTileTrans.y);
+    tileID = aTileID;
+    gl_Position = projection * view * model * vec4(aPos + offset, 0.0, 1.0);
+    TexCoords = aTexCoords;
+    instanceID = gl_InstanceID;
+}
---
+++ resources/www/js/app_library.js
@@ -0,0 +1,92 @@
+mergeInto(LibraryManager.library, {
+    getLocalTilemapFile: async function() {
+        const pickerOpts = {
+            types: [
+                {
+                    description: 'Image',
+                    accept: {
+                        'image/*': ['.jpeg', '.jpg', '.png']
+                    }
+                },
+            ],
+            excludeAcceptAllOption: true,
+            multiple: false,
+            _preferPolyfill: false // default
+        };
+        var contents;
+        var file;
+        try {
+            // Always returns an array.
+            const [handle] = await window.showOpenFilePicker(pickerOpts);
+            file = await handle.getFile();
+            contents = await file.arrayBuffer();
+            Module.openTilemapFile(file.name, file.type, contents, contents.byteLength);
+            //result = Module.ccall("int_sqrt", null, ["string", "array", "number"], [file.name, uint8View, contents.byteLength])
+
+            //Module.msgDebug(file);
+            console.log(file);
+            
+            return 0;
+    
+            // return handle.getFile();
+        } catch (err) {
+            console.error(err.name, err.message);
+            Module.msgError(err.message);
+        } finally {
+            // To avoid memory leaks we need to always clear out the allocated heap data
+            // This needs to happen in the finally block, otherwise thrown errors will stop code execution before this happens
+            Module._free(contents)
+            Module._free(file)
+        }
+        // Do something with the file handle.
+
+        return 1;
+    },
+    saveJSONFile: async function() {
+        try {
+            //var myUint8Array = Module.getJSON();
+            const rawStr = Module.ccall("getJSON", "string");
+            //var blob = new Blob(myUint8Array, {type: "text/json"});
+            //var string = new TextDecoder().decode(new Uint8Array(myUint8Array));
+            // create a new handle
+            handle = await window.showSaveFilePicker({
+                suggestedName: 'preset.json'
+            });
+
+            // create a FileSystemWritableFileStream to write to
+            const writableStream = await handle.createWritable();
+
+            await writableStream.write(rawStr);
+            await writableStream.close();
+
+            // write our file
+            //await writableStream.write(myUint8Array);
+            //console.log(blob);
+
+            // close the file and write the contents to disk.
+            // await writableStream.close();
+            // Module.message("Saving file: " + handle.name);
+        } catch (err) {
+            console.error(err.name, err.message);
+            Module.msgError(err.message);
+        } finally {
+            // To avoid memory leaks we need to always clear out the allocated heap data
+            // This needs to happen in the finally block, otherwise thrown errors will stop code execution before this happens
+            //Module._free(buffer)
+        }
+
+        return 1;
+    },
+    viewFullscreen: function() {
+        var elem = document.getElementById("canvas");
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) { /* Safari */
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE11 */
+            elem.msRequestFullscreen();
+        }
+
+        return 1;
+    },
+});
